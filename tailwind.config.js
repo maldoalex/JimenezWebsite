3e2097d0aea5b8dac0d138432ac9f32ce7458162
maldoalex <maldoalex@gmail.com>
2022-02-01 16:15:44 -0600

    navbar and hero section styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,30 @@ module.exports = {
     './layouts/*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-yellow": "#FEF08A",
+        "primary-text": "#292717",
+        "button-primary": "#544F2E",
+        "button-secondary": "#7E7845",
+        "secondary-yellow": "#D3C873",
+      },
+      // backgroundImage: {
+      //   "hero-desktop": "url('/images/landscapeShowcase.jpg')",
+      // }
+    },
+    fontFamily: {
+      Quicksand: 'Quicksand, sans-serif',
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1440px",
+        xl: "1440px",
+        "2xl": "1440px",
+      },
+    },
   },
   plugins: [],
 }
